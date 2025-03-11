bd6ce2cc351a8306d4dfe236dc769d9b93f21ec3
Karl <khjohansen97@hotmail.com>
2025-03-11 20:16:57 +0100

    update map show position + userpage

--- frontend/react-app/src/components/map/Map.tsx
+++ frontend/react-app/src/components/map/Map.tsx
@@ -5,7 +5,6 @@ import { useGeolocated } from "react-geolocated";
 import L from "leaflet";
 import MapLoader from "./MapLoader";
 
-// Custom marker icon to prevent missing icon issue
 const markerIcon = new L.Icon({
   iconUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
@@ -29,17 +28,17 @@ export const Map = () => {
     return <div>Enheten din støtter ikke geolokasjon 😢 </div>;
 
   if (!isGeolocationEnabled)
-    return <div>Kart krever at geolokasjon skrus på 😠 </div>;
+    return <div>Kart krever at posisjon deles 😠 </div>;
 
   if (!coords) return <MapLoader />;
 
   return (
     <MapContainer
-      // center={[
-      //   coords?.latitude || defaultLocation.lat,
-      //   coords?.longitude || defaultLocation.lng,
-      // ]}
-      center={[defaultLocation.lat, defaultLocation.lng]}
+      center={[
+        coords?.latitude || defaultLocation.lat,
+        coords?.longitude || defaultLocation.lng,
+      ]}
+      // center={[defaultLocation.lat, defaultLocation.lng]}
       zoom={17}
       style={{ height: "400px", width: "100%" }}
     >
@@ -51,7 +50,7 @@ export const Map = () => {
         position={[coords?.latitude, coords?.longitude]}
         icon={markerIcon}
       >
-        <Popup>Du er her</Popup>
+        <Popup>Din posisjon</Popup>
       </Marker>
       <Circle center={[coords?.latitude, coords?.longitude]} radius={100} />
       {/* {publicTrashLocations?.map((loc: any, index) => (
--- frontend/react-app/src/components/routing/pages/UserPage.tsx
+++ frontend/react-app/src/components/routing/pages/UserPage.tsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Heading = styled.h1`
   font-size: 3rem;
   color: #333;
@@ -9,8 +16,12 @@ const Heading = styled.h1`
 
 export const UserPage = () => {
   return (
-    <div>
+    <Container>
       <Heading>Min Side</Heading>
-    </div>
+      <h3>Masse info om kunden bla bla bla</h3>
+      <h3>Statistikk bla bla</h3>
+      <h3>knapper for å endre kundedata ..</h3>
+      <h3>hva mer? ..</h3>
+    </Container>
   );
 };
