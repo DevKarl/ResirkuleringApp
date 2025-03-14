c996958ee6ef801d1e2a15e7d5748bc54e139ea1
672654 <henrik_krane@hotmail.com>
2025-03-14 14:00:20 +0100

    lagt til klassen avfallstype

---
+++ backend/src/main/java/com/example/demo/Entities/AvfallsType.java
@@ -0,0 +1,16 @@
+package com.example.demo.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class AvfallsType {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String type;
+  
+}
