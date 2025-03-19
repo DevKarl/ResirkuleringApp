db8d8149128a04f806c78670c48e7318a2957df3
Karl <khjohansen97@hotmail.com>
2025-03-19 15:32:40 +0100

    remove testtable etc.

--- backend/src/main/java/com/example/demo/Repositories/TestTableRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entities.TestTable;
-
-public interface TestTableRepository extends JpaRepository<TestTable, Integer> {
-}
