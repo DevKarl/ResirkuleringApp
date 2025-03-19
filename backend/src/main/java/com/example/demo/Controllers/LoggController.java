98a5b5429fa45a4f1ab71fbe18692a0389a4ae2f
Karl <khjohansen97@hotmail.com>
2025-03-19 15:05:30 +0100

    replace old annontations with new ApiController

--- backend/src/main/java/com/example/demo/Controllers/LoggController.java
+++ backend/src/main/java/com/example/demo/Controllers/LoggController.java
@@ -1,10 +1,7 @@
 package com.example.demo.Controllers;
+import com.example.demo.Controllers.Interfaces.ApiController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
+@ApiController
 public class LoggController {
     
 }
