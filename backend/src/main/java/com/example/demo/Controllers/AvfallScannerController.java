c0ad49202f6a15bdf7e1106640ee3809ef7670d2
Karl <khjohansen97@hotmail.com>
2025-03-20 20:11:40 +0100

    rm whitespace

--- backend/src/main/java/com/example/demo/Controllers/AvfallScannerController.java
+++ backend/src/main/java/com/example/demo/Controllers/AvfallScannerController.java
@@ -1,16 +1,13 @@
 package com.example.demo.Controllers;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
 import com.example.demo.Controllers.Interfaces.ApiController;
 import com.example.demo.DTO.ScanAvfallResponse;
 import com.example.demo.Entities.Avfall;
 import com.example.demo.Entities.Avfallspunkt;
 import com.example.demo.Service.AvfPunktService;
 import com.example.demo.Service.AvfallService;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
