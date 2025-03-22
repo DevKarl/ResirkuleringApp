d9e0b9361c043e6d08d88204da739670227d57b3
672654 <henrik_krane@hotmail.com>
2025-03-22 11:47:45 +0100

    Spørring fungerer. Mangler brukerautetisering

--- backend/src/main/java/com/example/demo/Controllers/LoggController.java
+++ backend/src/main/java/com/example/demo/Controllers/LoggController.java
@@ -1,7 +1,25 @@
 package com.example.demo.Controllers;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.example.demo.Controllers.Interfaces.ApiController;
+import com.example.demo.Entities.Resirkuleringslogg;
+import com.example.demo.Service.LoggService;
+
 
 @ApiController
 public class LoggController {
     
+  @Autowired
+  private LoggService loggService;
+
+
+  @GetMapping("/getAlleLoggerForBrukerMedId")
+  public List<Resirkuleringslogg> getAlleLoggerForBrukerMedId(@RequestParam int id) {
+      return loggService.getAlleLoggerForBrukerMedId(id);
+  }
+  
 }
