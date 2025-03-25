53da51151ad397bf1f007ac2043e8c50cd4d1a68
Karl <khjohansen97@hotmail.com>
2025-03-25 01:12:21 +0100

    login wip

--- backend/src/main/java/com/example/demo/Controllers/BrukerController.java
+++ backend/src/main/java/com/example/demo/Controllers/BrukerController.java
@@ -1,7 +1,5 @@
 package com.example.demo.Controllers;
-
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
