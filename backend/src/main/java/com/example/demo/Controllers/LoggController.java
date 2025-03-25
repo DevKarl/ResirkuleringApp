857836b00eeca135ba42e4bc15567b23aaf02179
672654 <henrik_krane@hotmail.com>
2025-03-25 11:05:10 +0100

    getLoggerForInnloggetBruker med httpSession

--- backend/src/main/java/com/example/demo/Controllers/LoggController.java
+++ backend/src/main/java/com/example/demo/Controllers/LoggController.java
@@ -2,12 +2,14 @@ package com.example.demo.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.Controllers.Interfaces.ApiController;
 import com.example.demo.Entities.Resirkuleringslogg;
 import com.example.demo.Service.LoggService;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @ApiController
@@ -17,9 +19,25 @@ public class LoggController {
   private LoggService loggService;
 
 
-  @GetMapping("/getAlleLoggerForBrukerMedId")
-  public List<Resirkuleringslogg> getAlleLoggerForBrukerMedId(@RequestParam int id) {
-      return loggService.getAlleLoggerForBrukerMedId(id);
+  @GetMapping("/getAlleLoggerForInnloggetBruker")
+  public ResponseEntity<?> getAlleLoggerForBrukerForInnloggetBruker(HttpSession session) {
+
+    Object userId = session.getAttribute("userId");
+
+    if(userId == null) {
+    return ResponseEntity.status(401).body("Ingen bruker logget inn");
+    }
+    try {
+      List<Resirkuleringslogg> logger =  loggService.getAlleLoggerForBrukerMedId((Integer)userId);
+      if (logger.isEmpty()){
+        return ResponseEntity.status(404).body("Ingen logger funnet for bruker");
+      } else{
+        return ResponseEntity.ok(logger);
+      }
+     
+    } catch (Exception e) {
+      return ResponseEntity.status(500).body("Ops! Noe feil skjedde.");
+    } 
   }
-  
+ 
 }
