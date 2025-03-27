6b421bb9f5793b74deecde079e75db8fc5d3c24a
Karl <khjohansen97@hotmail.com>
2025-03-27 22:49:11 +0100

    rename getAlleLoggerForInnloggetBruker to getAvfallsLogg + small fix

--- backend/src/main/java/com/example/demo/Controllers/LoggController.java
+++ backend/src/main/java/com/example/demo/Controllers/LoggController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.Controllers.Interfaces.ApiController;
+import com.example.demo.DTO.ErrorResponse;
 import com.example.demo.Entities.Resirkuleringslogg;
 import com.example.demo.Service.LoggService;
 
@@ -18,23 +19,18 @@ public class LoggController {
   @Autowired
   private LoggService loggService;
 
-
-  @GetMapping("/getAlleLoggerForInnloggetBruker")
-  public ResponseEntity<?> getAlleLoggerForBrukerForInnloggetBruker(HttpSession session) {
-
+  @GetMapping("/getAvfallsLogg")
+  public ResponseEntity<?> getAvfallsLogg(HttpSession session) {
+    if (session == null) {
+      return ResponseEntity.badRequest().body(new ErrorResponse("Sesjonen er utløpt, vennligst logg inn på nytt."));
+    }
     Object userId = session.getAttribute("userId");
-
     if(userId == null) {
-    return ResponseEntity.status(401).body("Ingen bruker logget inn");
+      return ResponseEntity.status(401).body("Ingen bruker logget inn");
     }
     try {
       List<Resirkuleringslogg> logger =  loggService.getAlleLoggerForBrukerMedId((Integer)userId);
-      if (logger.isEmpty()){
-        return ResponseEntity.status(404).body("Ingen logger funnet for bruker");
-      } else{
-        return ResponseEntity.ok(logger);
-      }
-     
+      return ResponseEntity.ok(logger);
     } catch (Exception e) {
       return ResponseEntity.status(500).body("Ops! Noe feil skjedde.");
     } 
