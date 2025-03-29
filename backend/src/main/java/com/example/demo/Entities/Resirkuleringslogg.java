268b34ba32ce13a21a01425f643ec5491841d75a
Karl <khjohansen97@hotmail.com>
2025-03-30 00:06:09 +0100

    rm unused

--- backend/src/main/java/com/example/demo/Entities/Resirkuleringslogg.java
+++ backend/src/main/java/com/example/demo/Entities/Resirkuleringslogg.java
@@ -1,8 +1,5 @@
 package com.example.demo.Entities;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
